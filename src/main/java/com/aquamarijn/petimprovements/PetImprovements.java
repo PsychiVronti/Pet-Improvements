0e836f71c72e153b66a87669636c3252278f5393
Aquamarijn <bryanchau2702@gmail.com>
2025-05-22 00:12:01 -0500

    Initial commit

---
+++ src/main/java/com/aquamarijn/petimprovements/PetImprovements.java
@@ -0,0 +1,16 @@
+package com.aquamarijn.petimprovements;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PetImprovements implements ModInitializer {
+	public static final String MOD_ID = "petimprovements";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/aquamarijn/petimprovements/PetImprovementsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aquamarijn.petimprovements;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PetImprovementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
