ee8d46f715ae80c48e55210dbca8c9006ed5fcf7
Aquamarijn <bryanchau2702@gmail.com>
2025-06-13 03:33:21 -0700

    Preparing mod for release.

---
+++ src/main/java/com/aquamarijn/petimprovements/util/ModInfo.java
@@ -0,0 +1,6 @@
+package com.aquamarijn.petimprovements.util;
+
+public class ModInfo {
+    public static final String MOD_ID = "petimprovements";
+    public static final String VERSION = "@VERSION@";
+}
